d1a9175bedd3856a1428a9b0c8145ecaca446265
Rachit Thakker <rachit.0508@gmail.com>
2022-01-18 18:04:18 +0530

    Created custom component files and added to repositiory.

---
+++ src/components/End/End.js
@@ -0,0 +1,7 @@
+import "./End.css";
+
+const End = () => {
+  return <div></div>;
+};
+
+export default End;
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,13 @@
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <div className="intro-body">
+      <header>
+        <h1>PIZZA TIME</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Intro;
