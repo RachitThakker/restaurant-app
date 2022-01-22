6c2f0cc46b72c737feaf3ac0e366c840482d033b
Rachit Thakker <rachit.0508@gmail.com>
2022-01-23 00:52:42 +0530

    Item logic not working.

--- src/components/Main/Item.js
+++ src/components/Main/Item.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import "./Item.css";
+
 const Item = (props) => {
-  return <div className="item-container"></div>;
+  const [itemQuantity, setItemQuantity] = useState("0");
+  
+  const quantityHandler = event => {
+    setItemQuantity(event.target.value);
+    props.itemInfo.quantity = +(event.target.value);
+    console.log(props.itemInfo);
+  }
+  
+  return <div className="item-container">
+    <h4 className="item-name">{props.itemInfo.dishName}</h4>
+    <p className="item-description">{props.itemInfo.ingredients}</p>
+    <img className="item-image" src={props.itemInfo.imgUrl} alt={props.itemInfo.dishName} />
+    <h4 className="item-price">{props.itemInfo.price}</h4>
+    <input className="item-quantity" type="number" min="0" max="10" step="1" onChange={quantityHandler} />
+  </div>;
 };
 
 export default Item;
--- src/components/Main/ItemGroup.js
+++ src/components/Main/ItemGroup.js
@@ -7,6 +7,7 @@ const ItemGroup = (props) => {
   if (props.groupName === "Pizza") {
     items = [
       {
+        id: Math.random.toString(),
         dishName: "Margharita",
         ingredients: "Just cheese. Laziest pizza ever.",
         price: "89",
@@ -14,6 +15,7 @@ const ItemGroup = (props) => {
         quantity: "0",
       },
       {
+        id: Math.random.toString(),
         dishName: "Mexican Loaded",
         ingredients: "Jalapenos, Corn, Onion, Olives, Spicy Sauce.",
         price: "129",
@@ -21,6 +23,7 @@ const ItemGroup = (props) => {
         quantity: "0",
       },
       {
+        id: Math.random.toString(),
         dishName: "Pineapple Pizza",
         ingredients: "Really? No, Really?",
         price: "999",
@@ -33,6 +36,7 @@ const ItemGroup = (props) => {
   if (props.groupName === "Pasta") {
     items = [
       {
+        id: Math.random.toString(),
         dishName: "Alfredo Pasta",
         ingredients: "White Sauce, Bell Pepper, Capsicum, Jalapenos",
         price: "119",
@@ -40,6 +44,7 @@ const ItemGroup = (props) => {
         quantity: "0",
       },
       {
+        id: Math.random.toString(),
         dishName: "Tomato Pasta",
         ingredients: "Light Tomato Sauce, Onion, Corn, Olives",
         price: "109",
@@ -47,6 +52,7 @@ const ItemGroup = (props) => {
         quantity: "0",
       },
       {
+        id: Math.random.toString(),
         dishName: "Chef's Special Pasta",
         ingredients: "According to chef's mood which isn't always good.",
         price: "149",
@@ -59,6 +65,7 @@ const ItemGroup = (props) => {
   if (props.groupName === "Sandwich") {
     items = [
       {
+        id: Math.random.toString(),
         dishName: "Sweet Panini",
         ingredients: "Cabbage, Capsicum, Pineapple, Mayonaisse",
         price: "99",
@@ -66,6 +73,7 @@ const ItemGroup = (props) => {
         quantity: "0",
       },
       {
+        id: Math.random.toString(),
         dishName: "Aloo Tikki",
         ingredients: "Aloo Patty, Lettuce, Cabbage, Capsicum, Orange Sauce",
         price: "109",
@@ -73,6 +81,7 @@ const ItemGroup = (props) => {
         quantity: "0",
       },
       {
+        id: Math.random.toString(),
         dishName: "Paneer Love",
         ingredients: "Paneer Tikka, Lettuce, Tomatoes, Orange Sauce",
         price: "119",
@@ -88,7 +97,33 @@ const ItemGroup = (props) => {
     isExpanded ? setIsExpanded(false) : setIsExpanded(true);
   };
 
-  return (
+  if (!isExpanded) {
+    return (
+      <div>
+        {!isExpanded && (
+        <div className="groupItem-div__closed">
+          <div className="groupItem-div__closed__content">
+            <h3>{props.groupName}</h3>
+            <button className="button-expand" onClick={expandHandler}>
+              Show Items
+            </button>
+          </div>
+        </div>
+      )}
+      </div>
+    )
+  }
+
+  if (isExpanded) {
+    return (
+      <div>
+        {isExpanded &&
+        (items.forEach((item) => <Item itemInfo={item} onCollapse={expandHandler} /> ))}
+      </div>
+    )
+  }
+
+  /*return (
     <div>
       {!isExpanded && (
         <div className="groupItem-div__closed">
@@ -102,11 +137,11 @@ const ItemGroup = (props) => {
       )}
 
       {isExpanded &&
-        items.forEach((item) => {
+        (items.forEach((item) => {
           <Item itemInfo={item} onCollapse={expandHandler} />;
-        })}
+        }))}
     </div>
-  );
+  );*/
 };
 
 export default ItemGroup;
